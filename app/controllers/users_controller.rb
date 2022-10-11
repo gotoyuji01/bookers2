5f3fea7202db878e471ab1693ffa2392844193c2
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2022-10-11 16:09:30 +0000

    [Finish]課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,4 +34,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name,:profile_image,:introduction)
   end
   
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_path(current_user.id)) unless @user == current_user
+  end
+  
 end
